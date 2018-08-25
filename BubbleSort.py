2ec023b48cf8531dab0cb6273cb9d26971300692
milkrong <milkrong121@outlook.com>
2018-08-25 17:11:46 -0400

    20180825 update bubble sort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,15 +1,16 @@
 def bubble_sort(input_list):
-    sorted = False
+    is_sorted = False
     length = len(input_list) - 1
 
     while not sorted:
-        sorted = True #assume it is sorted
+        is_sorted = True  # assume it is sorted
         for i in range(length):
             if input_list[i] > input_list[i+1]:
-                sorted = False # find a position not sorted
+                is_sorted = False # find a position not sorted
                 input_list[i], input_list[i+1] = input_list[i+1], input_list[i]
 
     return input_list
+
 
 if __name__ == '__main__':
     my_list = [12, 5, 13, 8, 9, 65]
